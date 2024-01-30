768c6e2745d1db00e1bd44d1ebabd0d8cc8a6af3
shinhosuck <shinhosuck1973@gmail.com>
2024-01-30 20:00:01 +0800

    update

--- pet_blog/src/pages/LandingPage.js
+++ pet_blog/src/pages/LandingPage.js
@@ -163,17 +163,21 @@ function LandingPage() {
                 </div>
                 <div className="landing-page-posts-container">
                     <div className="landing-page-posts-wrapper">
-                        <h1 className='landing-page-posts-header'>Latest Posts</h1>
+                        <div className="landing-page-post-see-all-posts-container">
+                            <h1 className='landing-page-posts-header'>Latest Posts</h1>
+                            <Link to='/posts' className='landing-page-post-see-all-posts'>
+                                See all posts
+                                <i className="fa fa-arrow-right"></i>
+                            </Link>
+                        </div>
                         <div className="landing-page-posts">
                             {posts.map((post)=> {
                                 return (
-                                    <div
-                                        key={post.id} 
-                                        className="landing-page-post"
-                                    >
-                                        {/* <div className="landing-page-post-image-container">
+                                    <div key={post.id} className="landing-page-post">
+                                        <div className="landing-page-post-image-container">
                                             <img className='landing-page-post-image' src={post.image_url} alt={post.title} />
-                                        </div> */}
+                                        </div>
+                                        <div className="landing-page-post-color-overlay"></div>
                                         <div className='landing-page-post-text-content'>
                                             <div className="landing-page-post-author-and-date">
                                                 <h4 className='landing-page-post-author'>{post.author}</h4>
@@ -190,45 +194,37 @@ function LandingPage() {
                                                     Read more
                                                 </Link>
                                             </p>
-                                            <div className="landing-page-post-date-and-like">
-                                                {post.like.length > 1 ? 
-                                                    <div className='landing-page-post-like'>
-                                                        <div className='landing-page-post-like-container'>
-                                                            <i className="fa-solid fa-hands-clapping landing-page-post-like"></i>
-                                                            <span className='landing-page-post-like-count'>{post.like.length}</span>
-                                                            <span className='landing-page-post-like-text'>likes</span>
-                                                        </div>
-                                                        <div className='landing-page-post-num-of-replies-container'>
-                                                            <i className="fa-solid fa-message landing-page-post-num-of-post"></i>
-                                                            <span className='landing-page-post-reply-count'>{post.num_of_replies}</span>
-                                                            <span className='landing-page-post-reply-text'>{post.num_of_replies > 1 ? 'comments': 'comment'}</span>
-                                                        </div>
+                                            {post.like.length > 1 ? 
+                                                <div className='landing-page-post-like'>
+                                                    <div className='landing-page-post-like-container'>
+                                                        <i className="fa-solid fa-hands-clapping landing-page-post-clap"></i>
+                                                        <span className='landing-page-post-like-count'>{post.like.length}</span>
+                                                        <span className='landing-page-post-like-text'>likes</span>
                                                     </div>
-                                                : 
-                                                    <div className='landing-page-post-like'>
-                                                        <div className='landing-page-post-like-container'>
-                                                            <i className="fa-solid fa-hands-clapping landing-page-post-like"></i>
-                                                            <span className='landing-page-post-like-count'>{post.like.length}</span>
-                                                            <span className='landing-page-post-like-text'>like</span>
-                                                        </div>
-                                                        <div className='landing-page-post-num-of-replies-container'>
-                                                            <i className="fa-solid fa-message landing-page-post-num-of-post"></i>
-                                                            <span className='landing-page-post-reply-count'>{post.num_of_replies}</span>
-                                                            <span className='landing-page-post-reply-text'>{post.num_of_replies > 1 ? 'comments': 'comment'}</span>
-                                                        </div>
+                                                    <div className='landing-page-post-num-of-replies-container'>
+                                                        <i className="fa-regular fa-message landing-page-post-num-of-post"></i>
+                                                        <span className='landing-page-post-reply-count'>{post.num_of_replies}</span>
+                                                        <span className='landing-page-post-reply-text'>{post.num_of_replies > 1 ? 'comments': 'comment'}</span>
                                                     </div>
-                                                }
-                                            </div>
+                                                </div>
+                                            : 
+                                                <div className='landing-page-post-like'>
+                                                    <div className='landing-page-post-like-container'>
+                                                        <i className="fa-solid fa-hands-clapping landing-page-post-clap"></i>
+                                                        <span className='landing-page-post-like-count'>{post.like.length}</span>
+                                                        <span className='landing-page-post-like-text'>like</span>
+                                                    </div>
+                                                    <div className='landing-page-post-num-of-replies-container'>
+                                                        <i className="fa-regular fa-message landing-page-post-num-of-post"></i>
+                                                        <span className='landing-page-post-reply-count'>{post.num_of_replies}</span>
+                                                        <span className='landing-page-post-reply-text'>{post.num_of_replies > 1 ? 'comments': 'comment'}</span>
+                                                    </div>
+                                                </div>
+                                            }
                                         </div>
                                     </div>
                                 )
                             })}
-                        </div>
-                        <div className="landing-page-post-see-all-posts-container">
-                            <Link to='/posts' className='landing-page-post-see-all-posts'>
-                                See all posts
-                                <i className="fa fa-arrow-right"></i>
-                            </Link>
                         </div>
                     </div>
                 </div>
